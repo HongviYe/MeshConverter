4c0ad7198525f3c71c8bcd7320bb6774f11063a8
xqmeng <xqmeng@zju.edu.cn>
2021-10-27 23:32:04 +0800

    modify cmake, download eigen in cmake

---
+++ cmake/MeshConverterDownloadExternal.cmake
@@ -0,0 +1,30 @@
+################################################################################
+include(DownloadProject)
+
+if(NOT EXTERNAL_DIR)
+    set(EXTERNAL_DIR ${CMAKE_SOURCE_DIR}/extern)
+endif()
+
+if(NOT GITHUB_REPOSITE)
+    set(GITHUB_REPOSITE "github.com")
+endif()
+
+# Shortcut function
+function(meshconverter_download_project name)
+    download_project(
+        PROJ         ${name}
+        SOURCE_DIR   ${EXTERNAL_DIR}/${name}
+        DOWNLOAD_DIR ${EXTERNAL_DIR}/.cache/${name}
+        ${ARGN}
+    )
+endfunction()
+
+################################################################################
+
+## eigen
+function(meshconverter_download_eigen)
+    meshconverter_download_project(eigen
+        GIT_REPOSITORY https://${GITHUB_REPOSITE}/eigenteam/eigen-git-mirror.git
+        GIT_TAG        3.3.7
+    )
+endfunction()
